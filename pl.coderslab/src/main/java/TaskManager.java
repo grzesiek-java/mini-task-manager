a4ae4401f2360d0aceb3369c0de9c74355524e06
grzesiek-java <szadoq@gmail.com>
2021-05-21 13:14:02 +0200

    remove task

--- pl.coderslab/src/main/java/TaskManager.java
+++ pl.coderslab/src/main/java/TaskManager.java
@@ -1,7 +1,6 @@
 import org.apache.commons.lang3.ArrayUtils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
