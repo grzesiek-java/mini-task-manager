dbd590ccc87a505b87c22b400681cf45a77e1695
grzesiek-java <szadoq@gmail.com>
2021-05-21 11:13:23 +0200

    menu + read a file

--- pl.coderslab/src/main/java/TaskManager.java
+++ pl.coderslab/src/main/java/TaskManager.java
@@ -1,8 +1,42 @@
 import org.apache.commons.lang3.ArrayUtils;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class TaskManager {
+
     public static void main(String[] args) {
+        final String FILENAME = "tasks.csv";
+        menuPrint();
+        readFileToArray(FILENAME);
+
+
+    }
+
+    private static List readFileToArray(String file) {
+        List<String> tasks = new ArrayList<>();
+        Path p = Paths.get(file);
+
+        try {
+            tasks = Files.readAllLines(p);
+        } catch (IOException e) { System.out.println(e.getMessage()); }
+        return tasks;
+        }
+
+    private static void menuPrint() {
+        System.out.println(ConsoleColors.BLUE);
+        System.out.print("Please select an option: ");
+        System.out.println(ConsoleColors.RESET);
+        System.out.println("add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
     }
 }
