c2c0a4d9c5958995a93efaf881f1dec83e42e79f
grzesiek-java <szadoq@gmail.com>
2021-05-21 10:14:53 +0200

    test2

--- pl.coderslab/src/main/java/TaskManager.java
+++ pl.coderslab/src/main/java/TaskManager.java
@@ -4,8 +4,5 @@ import java.util.Arrays;
 
 public class TaskManager {
     public static void main(String[] args) {
-        String[] arr = {"test1","test2"};
-        arr = ArrayUtils.add(arr,"test3");
-        System.out.println(Arrays.toString(arr));
     }
 }
